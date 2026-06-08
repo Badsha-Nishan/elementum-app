8324cb9b5627878b5fcc5e7e171774727adb48f0
Badsha-Nishan <badshanisan14@gmail.com>
2026-06-09 00:15:35 +0600

    added all hero Ellips

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,6 +11,48 @@ const Hero = () => {
         Togeather, <br /> we belive that progress only hghappens when you refuse
         to play things safe.
       </p>
+      <div className="relative flex px-9 w-[1844px] border h-[453px]">
+        <img
+          className="absolute left-0 bottom-9"
+          src="./images/Ellipse1.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute left-38 top-28"
+          src="./images/Ellipse-2.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute top-0 left-128"
+          src="./images/Ellipse-3.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute left-158 bottom-0"
+          src="./images/Ellipse-4.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute top-12 left-232"
+          src="./images/Ellipse-5.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute top-32 left-272"
+          src="./images/Ellipse-6.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute top-0 left-342"
+          src="./images/Ellipse-7.png"
+          alt="CircleImage"
+        />
+        <img
+          className="absolute right-0 bottom-20"
+          src="./images/Ellipse-8.png"
+          alt="CircleImage"
+        />
+      </div>
     </section>
   );
 };
