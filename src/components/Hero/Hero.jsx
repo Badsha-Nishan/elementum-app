7764d3f819872eb7e90160ab1c5134fa44ebd1fe
Badsha-Nishan <badshanisan14@gmail.com>
2026-06-09 00:42:22 +0600

    implement completed the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,16 +2,42 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="relative text-center px-6 py-36">
-      <h1 className="text-4xl md:text-8xl font-gerbil leading-tight">
-        The thinkers and <br /> doers were changing <br /> the status Quo with
+    <section className="text-center px-6 py-36">
+      <h1 className="relative text-4xl md:text-8xl font-gerbil leading-tight">
+        The thinkers and <br /> doers were changing <br /> the
+        <span className="bg-[#D7EEDD] px-8 rounded-full">status</span>Quo with
       </h1>
+      <img
+        className="absolute top-108 right-32 -z-30"
+        src="./images/Ellipse6.png"
+        alt="Ellipse"
+      />
+      <img
+        className="absolute top-84 right-162 -z-30"
+        src="./images/Vector6.png"
+        alt="Ellipse"
+      />
+      <img
+        className="absolute top-90 right-72 -z-30"
+        src="./images/Ellipse7.png"
+        alt="Ellipse"
+      />
       <p className="text-2xl mt-7 mb-20">
         We are a team of strategists, designers communicators, researchers.
         Togeather, <br /> we belive that progress only hghappens when you refuse
         to play things safe.
       </p>
-      <div className="relative flex px-9 w-[1844px] border h-[453px]">
+      <div className="relative flex px-9 max-w-[1844px] min-h-[453px]">
+        <img
+          className="absolute -left-6 -top-56"
+          src="./images/orange.png"
+          alt="Curved Line"
+        />
+        <img
+          className="absolute left-8 -top-56"
+          src="./images/black.png"
+          alt="Curved Line"
+        />
         <img
           className="absolute left-0 bottom-9"
           src="./images/Ellipse1.png"
