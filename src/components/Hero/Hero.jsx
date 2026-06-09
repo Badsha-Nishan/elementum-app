a4023c036b83069fe5431e7a38007e21e89373b3
Badsha-Nishan <badshanisan14@gmail.com>
2026-06-09 10:35:29 +0600

    final update of hero section (responsive)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="text-center px-6 py-36">
-      <h1 className="relative text-4xl md:text-8xl font-gerbil leading-tight">
-        The thinkers and <br /> doers were changing <br /> the
+      {/* Title */}
+      <h1 className="relative text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-gerbil leading-tight">
+        The thinkers and <br className="lg:block hidden" /> doers were changing{" "}
+        <br className="hidden lg:block" /> the
         <span className="bg-[#D7EEDD] px-8 rounded-full">status</span>Quo with
       </h1>
       <img
@@ -22,12 +24,14 @@ const Hero = () => {
         src="./images/Ellipse7.png"
         alt="Ellipse"
       />
-      <p className="text-2xl mt-7 mb-20">
+      {/* Description */}
+      <p className="text-2xl mt-10 mb-20">
         We are a team of strategists, designers communicators, researchers.
-        Togeather, <br /> we belive that progress only hghappens when you refuse
-        to play things safe.
+        Together, <br className="hidden lg:block" /> we believe that progress
+        only happens when you refuse to play things safe.
       </p>
-      <div className="relative flex px-9 max-w-[1844px] min-h-[453px]">
+      {/* For Large Device */}
+      <div className="hidden lg:flex relative px-9 max-w-[1844px] min-h-[453px]">
         <img
           className="absolute -left-6 -top-56"
           src="./images/orange.png"
@@ -78,6 +82,27 @@ const Hero = () => {
           src="./images/Ellipse-8.png"
           alt="CircleImage"
         />
+      </div>
+      {/* For Small Devices */}
+      <div className="lg:hidden grid grid-cols-2 md:grid-cols-3 gap-3 relative px-9 max-w-[1844px] min-h-[453px]">
+        <img
+          className="absolute -z-10 -left-6 top-56"
+          src="./images/orange.png"
+          alt="Curved Line"
+        />
+        <img
+          className="absolute -z-10 left-8 top-56"
+          src="./images/black.png"
+          alt="Curved Line"
+        />
+        <img src="./images/Ellipse1.png" alt="CircleImage" />
+        <img src="./images/Ellipse-2.png" alt="CircleImage" />
+        <img src="./images/Ellipse-3.png" alt="CircleImage" />
+        <img src="./images/Ellipse-4.png" alt="CircleImage" />
+        <img src="./images/Ellipse-5.png" alt="CircleImage" />
+        <img src="./images/Ellipse-6.png" alt="CircleImage" />
+        <img src="./images/Ellipse-7.png" alt="CircleImage" />
+        <img src="./images/Ellipse-8.png" alt="CircleImage" />
       </div>
     </section>
   );
